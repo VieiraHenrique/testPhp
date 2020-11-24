002e949b159444b8dbafcd44827df053de219c74
massartval <massart.val@gmail.com>
2020-11-24 16:12:47 +0100

    added form in form.php and footer on all pages

--- form.php
+++ form.php
@@ -62,27 +62,47 @@
     <?php 
 
 if (isset($_GET['submit'])){
-    $city = $_GET['city'];
+  //  $city = $_GET['city'];
     echo "hello world";
-    echo $city;
+  //  echo $city;
 }
 
     ?>
     <main class="container">
       <div class="row">
         <div class="col">
-          <form action="" id="form">
-            <input type="text" name="city" id="city" placeholder="Enter minimum 2 letters">
-              <div>
-                <select id="display">
-                </select>
-              </div>
-            <input type="submit" id="submit" name="submit">
-         </form>
+          <div class="card">
+            <div class="card-title">
+              <h3>Form</h3>
+            </div>
+            <div class="card-body">
+              <p>For apartments, ignore "surface of the land"</p>
+              <form action="form.php" method="post">
+                <input type="text" name="postal_code" placeholder="postal_code" required="required" />
+                <input type="int" name="number_of_rooms" placeholder="number_of_rooms" required="required" />
+                <input type="int" name="house_area" placeholder="house_area in m2" required="required" />
+                <input type="text" name="fully_equipped_kitchen" placeholder="fully_equipped_kitchen" required="required" />
+                <input type="text" name="open_fire" placeholder="open_fire" required="required" />
+                <input type="text" name="terrace" placeholder="terrace" required="required" />
+                <input type="text" name="garden" placeholder="garden" required="required" />
+                <input type="int" name="surface_of_the_land" placeholder="surface_of_the_land in m2" required="required" />
+                <input type="int" name="number_of_facades" placeholder="number_of_facades" required="required" />
+                <input type="text" name="swimming_pool" placeholder="swimming_pool" required="required" />
+                <input type="text" name="state_of_the_building" placeholder="state_of_the_building" required="required" />
+                <input type="int" name="construction_year" placeholder="construction_year" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Predict</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </main>
-    <footer>
+    <footer class="container bg-dark text-light">
+      <div class="row">
+        <div class="col">
+          <p>ImmoEliza &copy; 2020</p>
+        </div>
+      </div>
     </footer>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
